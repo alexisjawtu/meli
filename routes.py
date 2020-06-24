1cc7dff8e130a6b51bd334f655f48f5ddb539882
alexisjawtu <ajawtu@dm.uba.ar>
2020-06-23 21:03:13 -0300

    loading function and schematized the distance

--- routes.py
+++ routes.py
@@ -1,5 +1,22 @@
 import json
+import math
 
 def load (demand):
     f = open (demand,)
     return json.load(f)
+
+def distance (label_one, label_two):
+    """ labels are MZ-1-003-008: 
+                   area-floor-aisle-position 
+
+        this distance takes everything as unitary """
+    aisles          = math.abs(int(label_one[5:8]) - int(label_two[5:8]))
+    diff_position   = math.abs(int(label_one[9:12]) - int(label_two[9:12]))
+    
+    crosses         = how many mults of 7 between ?
+    
+    correction      =  0 if same pari
+                      -1 if even--odd
+                      +1 if odd--even  
+
+    return 3*crosses + diff_position + 4*aisles + correction
