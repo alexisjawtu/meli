1fba22604b3343a76ff741417aaeb948a35b72f1
alexisjawtu <ajawtu@dm.uba.ar>
2020-06-26 14:59:55 -0300

    bug fix in the label distances

---
+++ test.py
@@ -0,0 +1,56 @@
+aisles 0
+diff_position 7
+horizontal_correction 0
+crosses 1
+1 MZ-1-001-007 ... MZ-1-001-021: 10
+
+
+aisles 2
+diff_position 10
+horizontal_correction 0
+crosses 2
+2 MZ-1-004-010 ... MZ-1-006-030: 24
+
+
+aisles 0
+diff_position 0
+horizontal_correction 0
+crosses 0
+3 MZ-1-005-021 ... MZ-1-005-021: 0
+
+
+aisles 0
+diff_position 1 OJO
+horizontal_correction 0
+crosses 0
+4 MZ-1-005-019 ... MZ-1-005-023: 1
+
+
+aisles 2
+diff_position 15
+horizontal_correction 0
+crosses 2
+5 MZ-1-001-039 ... MZ-1-003-009: 29
+
+
+aisles 2
+diff_position 1
+horizontal_correction 0
+crosses 0
+6 MZ-1-004-037 ... MZ-1-002-035: 15
+
+
+aisles 2
+diff_position 1 <<--------------OJO
+horizontal_correction 0
+crosses 0
+7 MZ-1-004-033 ... MZ-1-002-031: 13
+
+
+aisles 0
+diff_position 1
+horizontal_correction -1
+crosses 0
+8 MZ-1-004-033 ... MZ-1-004-034: 0
+
+
